9796ffdead47717712edc58f7f86e5222b075f87
TJacoob <tomas.j.martins@ist.utl.pt>
2018-07-25 04:09:23 +0100

    Landing Page Development
    - Menu Options and Scrolling implemented for the landing page

--- imports/ui/pages/index/index.js
+++ imports/ui/pages/index/index.js
@@ -4,17 +4,72 @@ import './index.html';
 //import '../../components/info/info.js';
 
 Template.index.onCreated(function () {
-
+	this.menuActive = new ReactiveVar( 0 );
 });
 
+Template.index.onRendered( function(){
+	
+	var temp = Template.instance()
+
+	var w1 = new Waypoint({
+		element: document.getElementById("landing-1"),
+		handler: function(direction) {
+			temp.menuActive.set(1);
+		},
+	});
+	var w2 = new Waypoint({
+		element: document.getElementById("landing-2"),
+		handler: function(direction) {
+			temp.menuActive.set(2);
+		},
+	});
+	var w3 = new Waypoint({
+		element: document.getElementById("landing-3"),
+		handler: function(direction) {
+			temp.menuActive.set(3);
+		},
+	});
+	var w4 = new Waypoint({
+		element: document.getElementById("landing-4"),
+		handler: function(direction) {
+			temp.menuActive.set(4);
+		},
+	});
+	var w5 = new Waypoint({
+		element: document.getElementById("landing-5"),
+		handler: function(direction) {
+			temp.menuActive.set(5);
+		},
+	});
+	
+
+})
+
 Template.index.helpers({
-  /*links() {
-    return Links.find({});
-  },*/
+	isMenuActive(opt)
+	{
+		//console.log(opt);
+		if (opt == Template.instance().menuActive.get())
+			return "menu-active";
+		else
+			return "menu-hover";
+	}
 });
 
 Template.index.events({
   	'click #scroll-landing': function(){
-    	console.log("Scroll");
-	}
+    	$('html, body').animate({
+        	scrollTop: $("#landing-1").offset().top 
+    	}, 1000);
+	},
+	
+	'click .scroll-menu': function(e,t)
+	{
+		let newopt = e.target.id ;
+		newopt = parseInt(newopt.split("menu-opt-")[1]);
+		//Template.instance().menuActive.set(newopt);
+		$('html, body').animate({
+        	scrollTop: $("#landing-"+newopt).offset().top + 10
+    	}, 1000);
+	},
 });
