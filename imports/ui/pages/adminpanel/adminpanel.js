b52d3e3915b6390085a8a14aace4c3e51f494360
TJacoob <tomas.j.martins@ist.utl.pt>
2018-07-27 16:37:55 +0100

    Admin Panel Structure
    - Set-up Admin Panel Structure to receive new templates with info

--- imports/ui/pages/adminpanel/adminpanel.js
+++ imports/ui/pages/adminpanel/adminpanel.js
@@ -1,1 +1,31 @@
 import './adminpanel.html';
+
+import './overview/overview.js';
+import './teams/teams.js';
+
+Template.adminPanel.onRendered(function(){
+	BlazeLayout.render('adminPanel', {ap_content:"ap_overview"});
+	Session.set("ap_activeMenu","ap_overview");
+})
+
+Template.adminPanel.helpers({
+	isActiveMenu(opt){
+		let active = Session.get("ap_activeMenu");
+		if ( "ap_"+opt == active )
+			return "menu-active";
+		else
+			return "menu-hover";
+	},
+})
+
+Template.adminPanel.events({
+	"click #ap_overview": function(){
+		BlazeLayout.render('adminPanel', {ap_content:"ap_overview"});
+		Session.set("ap_activeMenu","ap_overview");
+	},
+
+	"click #ap_teams": function(){
+		BlazeLayout.render('adminPanel', {ap_content:"ap_teams"});
+		Session.set("ap_activeMenu","ap_teams");
+	},
+})
