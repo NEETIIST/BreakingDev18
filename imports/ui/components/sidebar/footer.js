30849c944708e5ac67ae1a2cc8865075092d98ec
TJacoob <tomas.j.martins@ist.utl.pt>
2018-07-27 03:23:03 +0100

    Fully Implemented i18n
    - Sidebar now contains support to change language

--- imports/ui/components/sidebar/footer.js
+++ imports/ui/components/sidebar/footer.js
@@ -1,1 +1,27 @@
 import './footer.html';
+
+Template.footer.helpers({
+	isActiveLanguage(lang)
+	{
+		let cur = TAPi18n.getLanguage();
+		if (cur == lang)
+			return "fw-6";
+		else
+			return "fw-4";
+	}
+});
+
+Template.footer.events({
+	"click #lang-PT": function(){
+		console.log("here");
+		TAPi18n.setLanguage("pt");
+		//T9n.setLanguage("pt-PT");
+	},
+	"click #lang-EN": function(){
+		console.log("there");
+		TAPi18n.setLanguage("en");
+		//T9n.setLanguage("en");
+	},
+});
+
+	
